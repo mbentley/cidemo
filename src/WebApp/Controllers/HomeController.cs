54340c7c9b5d71f73681537814e2236d508bb3bc
Brandon Royal <broyal@docker.com>
2017-02-10 07:26:41 -0800

    cleanup

---
+++ src/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new EnvironmentData();
+            model.Hostname = System.Net.Dns.GetHostName();
+            return View(model);
+        }
+    }
+}
